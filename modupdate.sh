c3cd38a7ee37769d4f0d10235864c38c05a29938
Jonathan Kamens <jik@kamens.us>
2018-10-02 14:50:01 -0400

    Check remote IP of server sending moderator updates
    Now that we're publishing the code we use to maintain the moderator
    relays, it becomes possible for people to figure out how to hack them.
    Therefore, let's up the paranoia by confirming that the SMTP server we
    receive updates from matches the server we expect updates to come
    from.
    
    If it's necessary to submit an update by hand from a different server,
    e.g., because the update fails to process properly the first time due
    to a bug and we've fixed the bug and want to try again, then the
    script can be run with "--permissive" to suppress the remote IP check.

--- modupdate.sh
+++ modupdate.sh
@@ -1,11 +1,47 @@
 #!/bin/bash -ex
 
+tf=/tmp/modupdate.$$
+list_host=lists.eyrie.org
 target=$HOME/aliases/aliases
 now=$(date +%Y%m%d%H%M%S)
 old=$HOME/aliases/old/aliases.$now
 new=$target.$now
+check_ip=true
 
-sed -n '/Beginning of list of aliases/,/End of list of aliases/p' > $new
+while [ -n "$1" ]; do
+    case "$1" in
+        --permissive) shift; check_ip=false ;;
+        *) echo "Unrecognized argument \"$1\"" 1>&2; exit 1 ;;
+    esac
+done
+
+trap "rm -f $tf" EXIT
+rm -f $tf
+cat > $tf
+
+if $check_ip; then
+    received=$(formail -x Received -c < $tf | head -1)
+    if [ ! "$received" ]; then
+        echo "Could not find Received line in message" 1>&2
+        exit 1
+    fi
+
+    remote_ip=$(echo "$received" | sed -n -E \
+                -e 's/.*\[([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\].*/\1/p' \
+                -e 's/.*\[IPv6:([0-9a-f:]+)\].*/\1/p')
+    if [ ! "$remote_ip" ]; then
+        echo "Could not find remote IP in first Received line" 1>&2
+        exit 1
+    fi
+
+    matching=$(host $list_host | awk '$NF == "'$remote_ip'" {print}')
+    if [ ! "$matching" ]; then
+        echo "Remote IP $remote_ip does not appear to match $list_host" 1>&2
+        exit 1
+    fi
+fi
+
+sed -n '/Beginning of list of aliases/,/End of list of aliases/p' < $tf > $new
 if [ ! -s $new ]; then
     echo "Empty after sed" 1>&2
     exit 1
