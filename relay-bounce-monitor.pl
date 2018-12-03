86ec81a8c48186bb508ebed8277a376fa4fdea63
Jonathan Kamens <jik@kamens.us>
2018-12-03 11:14:11 -0500

    Filter out more spam error messages

--- relay-bounce-monitor.pl
+++ relay-bounce-monitor.pl
@@ -67,7 +67,8 @@ while (<>) {
 	     spam\ score\ exceeded|cmuScsDiscardSpam|
 	     Message\ contained\ unsafe\ content|spam\ message\ rejected|
 	     This\ message\ was\ blocked\ because\ its\ content|
-             Domain\ Blacklist:\ DBL\ lookup|message\ looks\ like\ spam/x);
+             Domain\ Blacklist:\ DBL\ lookup|message\ looks\ like\ spam|
+             is\ likely\ unsolicited\ mail|spam\ score\ too\ high/x);
     # I use this when looking for new permanent failure messages, but normally
     # it should be commented out because otherwise the script won't ever report
     # anything!
